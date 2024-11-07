6e55d65c724dc4f6cce98c17f74e412c891e2b41
Jon Rafkind <jon@rafkind.com>
2024-11-06 22:20:12 -0800

    add undo log

--- js/code.js
+++ js/code.js
@@ -12,23 +12,52 @@ function makePlayer(id){
 let player1 = makePlayer(1);
 let player2 = makePlayer(2);
 
+let undoLog = [];
+
 function elem(id){
     return document.getElementById(id);
 }
 
+function undo(){
+    if (undoLog.length > 0){
+        undoLog.pop()();
+    }
+}
+
+function updateStats(player){
+    elem(`player${player.id}-score`).innerText = player.score;
+    elem(`player${player.id}-winners`).innerText = player.stats.winners;
+    elem(`player${player.id}-errors`).innerText = player.stats.errors;
+}
+
 function incrementScore(player, amount){
     player.score += amount;
-    elem(`player${player.id}-score`).innerText = player.score;
+    updateStats(player);
+
+    undoLog.push(() => {
+        player.score -= amount;
+        updateStats(player);
+    })
 }
 
 function incrementWinners(player){
     player.stats.winners += 1;
-    elem(`player${player.id}-winners`).innerText = player.stats.winners;
+    updateStats(player);
+
+    undoLog.push(() => {
+        player.stats.winners -= 1;
+        updateStats(player);
+    })
 }
 
 function incrementErrors(player){
     player.stats.errors += 1;
-    elem(`player${player.id}-errors`).innerText = player.stats.errors;
+    updateStats(player);
+
+    undoLog.push(() => {
+        player.stats.errors -= 1;
+        updateStats(player);
+    })
 }
 
 function incrementPlayer1Score(){
